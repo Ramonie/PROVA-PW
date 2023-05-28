8d9c8dd23a42e0104d4e3a342529670a891c7bb6
Ramonie <ramonmartinsmr@gmail.com>
2023-05-28 12:06:31 -0300

    versaõ 1.0

--- comida/comida/src/main/java/pw/comida/controller.java
+++ comida/comida/src/main/java/pw/comida/controller.java
@@ -1,13 +1,23 @@
-package pw.comida;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controller {
-    @GetMapping("/")
-    public String index() {
+public class IndexController {
+    
+    @Autowired
+    private ItemRepository itemRepository;
+    
+    @GetMapping("/index")
+    public String getIndex(Model model) {
+        // Buscar todos os itens não deletados do banco de dados
+        List<Item> items = itemRepository.findByDeletedIsNull();
+        
+        // Adicionar a lista de itens ao modelo
+        model.addAttribute("items", items);
+        
+        // Retornar o nome do template Thymeleaf que será usado para renderizar a página
         return "index";
     }
-
 }
